2913f1ed3638c1048b51bf866129ad5b66bd562b
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2025-12-16 21:01:03 +0100

    setup express server & MVC structure

--- flights/backend/src/controllers/auth.controller.js
+++ flights/backend/src/controllers/auth.controller.js
@@ -1,28 +1,29 @@
-import db from "../config/db.js";
+import db from "../db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import RefreshToken from "../models/refreshToken.mongo.js";
+import {
+  ACCESS_SECRET,
+  REFRESH_SECRET,
+  ACCESS_EXPIRES,
+  REFRESH_EXPIRES
+} from "../config/jwt.js";
 
 export const register = async (req, res) => {
   const { firstName, lastName, email, password, gender, birthday } = req.body;
 
-  const [exists] = await db.query(
-    "SELECT id FROM users WHERE email = ?",
-    [email]
-  );
-
-  if (exists.length > 0) {
-    return res.status(400).json({ error: "Email already exists" });
-  }
+  const [exists] = await db.query("SELECT id FROM users WHERE email = ?", [email]);
+  if (exists.length) return res.status(400).json({ error: "Email already exists" });
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const [result] = await db.query(
-    `INSERT INTO users 
-     (first_name, last_name, email, password, gender, birthday)
+    `INSERT INTO users (first_name, last_name, email, password, gender, birthday)
      VALUES (?, ?, ?, ?, ?, ?)`,
     [firstName, lastName, email, hashedPassword, gender, birthday]
   );
 
-  res.json({
+  res.status(201).json({
     user: {
       id: result.insertId,
       firstName,
@@ -37,30 +38,34 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
-  const [users] = await db.query(
-    "SELECT * FROM users WHERE email = ?",
-    [email]
-  );
-
-  if (users.length === 0) {
-    return res.status(404).json({ error: "User not found. Please sign up." });
-  }
+  const [users] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+  if (!users.length) return res.status(404).json({ error: "User not found" });
 
   const user = users[0];
   const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(401).json({ error: "Invalid password" });
 
-  if (!match) {
-    return res.status(401).json({ error: "Invalid password" });
-  }
+  const accessToken = jwt.sign(
+    { id: user.id, role: user.role },
+    ACCESS_SECRET,
+    { expiresIn: ACCESS_EXPIRES }
+  );
+
+  const refreshToken = jwt.sign(
+    { id: user.id },
+    REFRESH_SECRET,
+    { expiresIn: REFRESH_EXPIRES }
+  );
+
+  await RefreshToken.create({ userId: user.id, token: refreshToken });
 
   res.json({
+    accessToken,
+    refreshToken,
     user: {
       id: user.id,
-      firstName: user.first_name,
-      lastName: user.last_name,
       email: user.email,
-      gender: user.gender,
-      birthday: user.birthday,
+      role: user.role,
     },
   });
 };
