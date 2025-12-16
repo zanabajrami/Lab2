4d6f4ab0c0c882e505d242f7d8fda438e5e24621
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2025-12-16 21:02:36 +0100

    setup MySQL users table

--- flights/backend/src/config/db.js
+++ flights/backend/src/config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "flights",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default db;
