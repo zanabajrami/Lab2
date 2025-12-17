48bd18fa488a8abda8590d70df6a81cfdbdc2163
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2025-12-17 20:28:33 +0100

    initialized server and implemented JWT authentication

---
+++ flights/backend/src/config/jwt.js
@@ -0,0 +1,5 @@
+export const ACCESS_SECRET = process.env.ACCESS_SECRET;
+export const REFRESH_SECRET = process.env.REFRESH_SECRET;
+
+export const ACCESS_EXPIRES = "15m";
+export const REFRESH_EXPIRES = "7d";
--- flights/backend/src/server.js
+++ flights/backend/src/server.js
@@ -1,69 +1,21 @@
 import express from "express";
 import cors from "cors";
-import db from "./db.js"; // db.js që e ke bërë
-import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import { connectMongo } from "./config/mongo.js";
+
+dotenv.config();
 
 const app = express();
+
 app.use(cors());
-app.use(express.json()); // për të lexuar JSON nga body
+app.use(express.json());
 
-// REGISTER
-app.post("/api/auth/register", async (req, res) => {
-  const { firstName, lastName, email, password, gender, birthday } = req.body;
+connectMongo();
 
-  try {
-    const [exists] = await db.query("SELECT id FROM users WHERE email = ?", [email]);
-    if (exists.length > 0) return res.status(400).json({ error: "Email already exists" });
+app.use("/api/auth", authRoutes);
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const [result] = await db.query(
-      `INSERT INTO users (first_name, last_name, email, password, gender, birthday) VALUES (?, ?, ?, ?, ?, ?)`,
-      [firstName, lastName, email, hashedPassword, gender, birthday]
-    );
-
-    res.json({
-      user: {
-        id: result.insertId,
-        firstName,
-        lastName,
-        email,
-        gender,
-        birthday,
-      },
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
-  }
-});
-
-// LOGIN
-app.post("/api/auth/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const [users] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
-    if (users.length === 0) return res.status(404).json({ error: "User not found" });
-
-    const user = users[0];
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(401).json({ error: "Invalid password" });
-
-    res.json({
-      user: {
-        id: user.id,
-        firstName: user.first_name,
-        lastName: user.last_name,
-        email: user.email,
-        gender: user.gender,
-        birthday: user.birthday,
-      },
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
-  }
-});
-
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Backend running on http://localhost:${PORT} 🚀`)
+);
