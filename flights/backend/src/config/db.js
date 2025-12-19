d720a0edfe12c5b799983b67eed0d62e2a4742ad
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2025-12-19 23:19:27 +0100

    Setup database connections: MongoDB and MySQL

--- flights/backend/src/config/db.js
+++ flights/backend/src/config/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "flights",
+  port: 3306,
 });
 
 export default db;
