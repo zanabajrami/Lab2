c03a115bdfd878853b6fce20c358a8650687dc9c
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2025-12-20 23:59:34 +0100

    Added role middleware

---
+++ flights/backend/src/middleware/role.middleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { ACCESS_SECRET } from "../config/jwt.js";
+
+export const verifyAdmin = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: "Not authenticated" });
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, ACCESS_SECRET);
+
+    if (decoded.role !== "admin") {
+      return res.status(403).json({ error: "Access denied. Admins only." });
+    }
+
+    req.user = decoded; // ruaj info të user-it për controller
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
