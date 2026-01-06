31605b14c2c1312414b74e7d6176d95895c185a0
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-01-06 18:52:43 +0100

    Update admin middleware

--- flights/backend/src/middleware/admin.middleware.js
+++ flights/backend/src/middleware/admin.middleware.js
@@ -1,29 +1,19 @@
 import jwt from "jsonwebtoken";
-import { ACCESS_SECRET } from "../config/jwt.js";
+import { ACCESS_SECRET } from "../config/jwt.js"; 
 
-// Verify JWT Token
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({ message: "Not authenticated" });
-  }
+  if (!authHeader) return res.status(401).json("Not authenticated");
 
   const token = authHeader.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, ACCESS_SECRET);
-    req.user = decoded; // { id, role }
+  jwt.verify(token, ACCESS_SECRET, (err, user) => {
+    if (err) return res.status(403).json("Token not valid");
+    req.user = user;
     next();
-  } catch (err) {
-    return res.status(403).json({ message: "Invalid token" });
-  }
+  });
 };
 
-// Verify Admin Role
 export const verifyAdmin = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Admins only" });
-  }
+  if (req.user.role !== "admin") return res.status(403).json("Access denied");
   next();
 };
