85ffe31f8dc40fa1840b28df09b6b27ddcd9ff3d
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-01-06 18:55:45 +0100

    Add users routes to server.js

--- flights/backend/src/server.js
+++ flights/backend/src/server.js
@@ -2,28 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+
+import authRoutes from "./routes/auth.routes.js";
 import usersRoutes from "./routes/users.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 dotenv.config();
 
-import authRoutes from "./routes/auth.routes.js";
-import adminRoutes from "./routes/admin.routes.js";
-
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
-
-// Lidhja me MongoDB
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected ✅"))
-.catch(err => console.error("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
+// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
 app.use("/api/users", usersRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(8800, () => console.log("Server running on port 8800"));
