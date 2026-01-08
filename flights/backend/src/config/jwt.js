88a95c149f68afe7f811a0eea88fcfa76e97e887
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-01-08 23:56:29 +0100

    Changed access token

--- flights/backend/src/config/jwt.js
+++ flights/backend/src/config/jwt.js
@@ -1,4 +1,4 @@
 export const ACCESS_SECRET = process.env.ACCESS_SECRET;
 export const REFRESH_SECRET = process.env.REFRESH_SECRET;
-export const ACCESS_EXPIRES = "15m";
+export const ACCESS_EXPIRES = "60m";
 export const REFRESH_EXPIRES = "7d";
