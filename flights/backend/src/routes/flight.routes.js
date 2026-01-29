97683aed97bb014c14f6725a1c8a0e2a1c4f855d
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-01-29 22:59:33 +0100

    Remove files

--- flights/backend/src/routes/flight.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getFlights, addFlight } from "../controllers/flight.controller.js";
-
-const router = express.Router();
-
-router.get("/", getFlights);
-router.post("/", addFlight);
-
-export default router;
