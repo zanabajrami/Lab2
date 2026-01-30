ab5f4ed5acb2c07bf8d69ce4113e06f63c319c19
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-01-30 22:36:39 +0100

    Add flights routes

---
+++ flights/backend/src/routes/flights.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  getFlights,
+  createFlight,
+  updateFlight,
+  deleteFlight,
+} from "../controllers/flights.controller.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", verifyToken, getFlights);
+router.post("/", verifyToken, createFlight);
+router.put("/:id", verifyToken, updateFlight);
+router.delete("/:id", verifyToken, deleteFlight);
+
+export default router;
